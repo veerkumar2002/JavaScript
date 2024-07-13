a9cca27140805b47d5ce60b170dea94c27670a7d
Chandraveer Kumar <142979517+veerkumar2002@users.noreply.github.com>
2024-07-13 12:19:19 +0000

    dataConversion

---
+++ 01_basics/03_dataConversion.js
@@ -0,0 +1,24 @@
+let score = "33abcd"
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1 ; false = 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
