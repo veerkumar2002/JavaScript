3136d2891ec673927917a28103dc0d3b92e99be2
Chandraveer Kumar <142979517+veerkumar2002@users.noreply.github.com>
2024-07-13 06:21:18 +0000

    let, const and var

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 141341
+let accountEmail = "veer@gmail.com"
+var accountPassword = "123"
+accountCity = "Bihar"
+let accountState; // ye print krke undefined dega
+
+//accountId = 2 not allowed
+
+accountEmail = "chand@gmail.com"
+accountPassword = " 24545"
+accountCity = "Bengsluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
