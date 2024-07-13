b7821d87492c0bc5f7a1eb9228b2920e5e997f5b
Chandraveer Kumar <142979517+veerkumar2002@users.noreply.github.com>
2024-07-13 13:20:26 +0000

    operationConverstion

---
+++ 01_basics/04_operationConversion.js
@@ -0,0 +1,29 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// add string
+
+let str1 = "hello"
+let str2 = " veer"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
