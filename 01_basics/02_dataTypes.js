e624e3172a046453856f6aaaaf8b7c9a770c9507
Chandraveer Kumar <142979517+veerkumar2002@users.noreply.github.com>
2024-07-13 07:20:17 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+//alert( 3 + 3) // we are using nodejs, not browser
+ console.log(3 + 
+    3) // code readability should be high
+
+    console.log("Veer")
+
+    let name = "veer"
+    let age = 18
+    let isLoggedIn = true
+
+    // number => 2 to power 53
+    // bigint 
+    // string => ""
+    //boolean => trur/false
+    // null => standalone value
+    // undefined =>
+    // symbol => uniqe
+    // object 
+
+    console.log(typeof "veer");// string
+    console.log(typeof null); //object
+    console.log(typeof undefined); //undefined
