f523330209a2b2e3a72cfc7f08ce7a4a2dbd67c2
Chandraveer Kumar <142979517+veerkumar2002@users.noreply.github.com>
2024-07-18 20:21:44 +0000

    Stack&Heap

---
+++ 01_basics/stackandheap.js
@@ -0,0 +1,19 @@
+let myYoutubename = "hiteshchoudharycom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "veer@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
