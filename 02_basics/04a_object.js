1db30ac3445f07ebf806c9e2e4edaaed67961376
Chandraveer Kumar <142979517+veerkumar2002@users.noreply.github.com>
2024-10-10 20:21:56 +0000

    de-structuring

---
+++ 02_basics/04a_object.js
@@ -0,0 +1,14 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitest"
+}
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "ja in hindi", // this fromate is json formate
+//     "price": "free"
+// }
