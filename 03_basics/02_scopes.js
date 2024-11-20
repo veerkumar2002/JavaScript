e43f336dfa43a178e2060783ceba9c2c3206ddf1
Chandraveer Kumar <142979517+veerkumar2002@users.noreply.github.com>
2024-11-20 09:20:16 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,55 @@
+
+//{} - this is scope
+//var c = 300
+
+let a = 300
+
+if(true){
+    let a = 10
+    const b = 20
+    //console.log("INNER: ", a);
+
+}
+
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(){
+    const username = "veer"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);  -- this is provide error because this line out of the scopes
+
+    two()
+}
+
+//one()
+
+if(true){
+    const username = "veer"
+    if(username === "veer"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+// ++++++++++++++ intersting ++++++++++++
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
