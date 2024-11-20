fc2e8722a888878e11c341440645f32429083393
Chandraveer Kumar <142979517+veerkumar2002@users.noreply.github.com>
2024-11-20 08:22:20 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -41,3 +41,35 @@ function loginUserMessage(username){
 //console.log(loginUserMessage("Veer"))
 //console.log(loginUserMessage(""))
 console.log(loginUserMessage("veer"))//gives output undefined
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 500, 1000))
+
+const user = {
+    username: "Veer",
+    prices: 200
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+    username: "sam",
+    price: 400
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 400, 500, 1000]));
